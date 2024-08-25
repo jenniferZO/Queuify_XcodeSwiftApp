ddef8b54d8f82e77e392750f7315faca840578d8
jenniferZO <153380641+jenniferZO@users.noreply.github.com>
2024-08-25 10:32:13 +0100

    Update UserEntity+CoreDataClass.swift

--- UserEntity+CoreDataClass.swift
+++ UserEntity+CoreDataClass.swift
@@ -1,10 +1,3 @@
-//
-//  UserEntity+CoreDataClass.swift
-//  Queue
-//
-//  Created by Zhao, Jennifer (OXF) Student on 11/07/2024.
-//
-//
 
 import Foundation
 import CoreData
@@ -13,4 +6,5 @@ import CoreData
 public class UserEntity: NSManagedObject {
     @NSManaged public var userIDE: String?
 }
+
 
